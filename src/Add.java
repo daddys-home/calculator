3ba5df07b58a6356afb4a2990298b709219bc006
TS-iii <jttesung@naver.com>
2019-07-03 17:06:40 +0900

    Add function catch overflow

--- src/Add.java
+++ src/Add.java
@@ -3,12 +3,19 @@ public class Add {
 	
 	private int a,b;
 	
+	
 	public int Add(int x,int y)
 	{
 		
 		a=x;
 		b=y;
 		
+		if(x>Integer.MAX_VALUE-y)
+		{
+			return -1;
+			
+		}
+	
 		return a+b;
 		
 		
