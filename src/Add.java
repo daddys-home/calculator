1e4d0110519da99c076805e91afa59c7dd634552
TS-iii <jttesung@naver.com>
2019-07-03 17:15:15 +0900

    revise Add Class

--- src/Add.java
+++ src/Add.java
@@ -1,26 +1,5 @@
-public class Add {
-	
-	
-	private int a,b;
-	
-	
-	public int Add(int x,int y)
-	{
-		
-		a=x;
-		b=y;
-		
-		if(x>Integer.MAX_VALUE-y)
-		{
-			return -1;
-			
-		}
-	
-		return a+b;
-		
-		
-	}
-	
-	
+public int Add(int inputnum1,int inputnum2,)
+{
 
+	return  inputnum1+  inputnum2,;
 }
